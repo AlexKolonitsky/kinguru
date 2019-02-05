101e4feaab87d8fd0541836bd5844498b664d3f3
KirylTytsyk <kiryltytsyk@gmail.com>
2019-02-05 18:26:00 +0300

    Add modal

--- js/2_meetup.js
+++ js/2_meetup.js
@@ -322,4 +322,29 @@
     return false;
   });
 
+    $(document).ready(function() {
+        $('#open-rating').click( function(event){
+            event.preventDefault();
+            $('#overlay').fadeIn(400,
+                function(){
+                    $('#modal_form')
+                        .css('display', 'block')
+                        .animate({opacity: 1, top: '50%'}, 200);
+                });
+        });
+        $('#modal_close, #overlay').click( function(){
+            $('#modal_form')
+                .animate({opacity: 0, top: '45%'}, 200,
+                    function(){
+                        $(this).css('display', 'none');
+                        $('#overlay').fadeOut(400);
+                    }
+                );
+        });
+    });
+
+    $(function(){
+        $("#includedContent").load('rating.html');
+    });
+
 })(jQuery); // End of use strict
