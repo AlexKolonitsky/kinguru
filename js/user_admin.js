eabf32898152124314e7d33e18d7cff209af59ac
NastassiaGorbunova <nastassia.gorbunova@mifort.org>
2019-02-06 15:58:31 +0300

    add: user-admin panel

---
+++ js/user_admin.js
@@ -0,0 +1,53 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Tabs
+  $(function(){
+    $("#myTab a").click(function(e){
+      e.preventDefault();
+      $(this).tab('show');
+    });
+  });
+
+
+  // Closes responsive menu and change burger icon when a scroll trigger link is clicked or click
+  // outside
+  $(".js-scroll-trigger").click(function() {
+    $(".navbar-collapse").collapse("hide");
+    $("#nav-icon1").toggleClass("open");
+  });
+
+  $(document).click(function(event) {
+    if($("#navbarResponsive").hasClass("show")) $("#nav-icon1").toggleClass("open");
+    if ($(event.target).closest(".js-scroll-trigger").length) return;
+    $(".navbar-collapse").collapse("hide");
+    event.stopPropagation();
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: 110
+  });
+
+  $(document).ready(function() {
+    $("#burger-menu-button").click(function() {
+      $("#nav-icon1").toggleClass("open");
+    });
+  });
+
+
+  // LikeBTN
+  $(document).ready(function(){
+    $('.side').on('click', function(){
+      if($(this).hasClass('active')){
+        $(this).removeClass('active');
+        $(this).addClass('unactive');
+      } else {
+        $(this).removeClass('unactive');
+        $(this).addClass('active');
+      }
+    })
+  });
+
+})(jQuery); // End of use strict
