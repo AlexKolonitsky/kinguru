3f56c19f1644fdb9a1638d2761a7fdd8690ce718
Arthur <arthur.bazhanov@mifort.org>
2019-02-15 11:25:45 +0300

    remove staticserver folder

--- staticserver/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const express = require('express');
-const PORT = 3010;
-const app = express();
-
-app.use('/', express.static(__dirname + '/public' ));
-
-app.listen(PORT, () => console.log(`Server run on port: ${PORT}`));
