e3123112dabc20134421f40394200fd4aabd8252
KirylTytsyk <kiryltytsyk@gmail.com>
2019-02-18 17:43:41 +0300

    change new_table

---
+++ frontend/js/new_table.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('.bike').click(function(){
+    $('.bike-open').stop().fadeToggle();
+  });
+});
+
+$(function() {
+  $('.boat').click(function(){
+    $('.boat-open').stop().fadeToggle();
+  });
+});
