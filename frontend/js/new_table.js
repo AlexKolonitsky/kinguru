18e8600d6f092a171fa72f043af70e7cce865d5a
KirylTytsyk <kiryltytsyk@gmail.com>
2019-02-19 17:32:46 +0300

    add cos prise. change size guest and speakers radio

--- frontend/js/new_table.js
+++ frontend/js/new_table.js
@@ -9,3 +9,37 @@ $(function() {
     $('.boat-open').stop().fadeToggle();
   });
 });
+
+$(function() {
+  $('.switch1').click(function(){
+    $('.price-open').stop().fadeToggle();
+  });
+});
+
+
+
+$(document).ready(function() {
+  $('.switch2').click(function(){
+    var isDisabled = $('.speakers').is(':disabled');
+    if (isDisabled) {
+      $('.label-speaker').removeClass('disable-radio');
+      $('.speakers').prop('disabled', false);
+      console.log('ne');
+      return;
+    }
+    $('.label-speaker').addClass('disable-radio');
+    $('.speakers').prop('disabled', true);
+    console.log('da');
+
+  });
+
+  $(document).ready(function() {
+    $(":radio").on("click", function() {
+      var dataClass = $(this).data("class");
+      $(".guest-number").each(function() {
+        this.disabled = !$(this).hasClass(dataClass);
+      });
+    });
+  });
+
+});
