164f99c6d2ddecb939e45f696bb13e16614412d9
AlexanderBeichuk <alexander.beichuk@gmail.com>
2019-02-25 17:47:35 +0300

    Get recent meetups

--- backend/dao/meetups/Meetups.js
+++ backend/dao/meetups/Meetups.js
@@ -56,7 +56,7 @@ module.exports = (sequelize, DataTypes) => {
     timestamps: true,
   });
 
-  Meetups.associate= models =>{
+  Meetups.associate = models => {
 
     Meetups.belongsToMany(models.Speakers, {
       as: 'speakers',
