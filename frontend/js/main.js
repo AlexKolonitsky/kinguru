f7889faca3721401e306ad367cb08a7a335fc57f
iralitv <ira.litvinchuk@mifort.org>
2019-03-21 21:40:10 +0300

    + slick-slider 1_meetups

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -403,6 +403,16 @@
     });
   });
 
+  $(document).ready(function(){
+    $('.your-class').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+  });
+
 })(jQuery);
 
 var like = function (element) {
