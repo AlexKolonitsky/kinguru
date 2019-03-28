e0ef8c61b96c5a77e0f2121e24458b433e0a5ebc
KirylTytsyk <kiryltytsyk@gmail.com>
2019-03-28 16:29:45 +0300

    bug fix multiselect

--- frontend/js/new_table.js
+++ frontend/js/new_table.js
@@ -11,21 +11,6 @@ $.ajax({
   contentType: "application/json; charset=utf-8",
   success: function (jsondata) {
     speakerJobTitle(jsondata);
-    $('#speakerJobTitle').bsMultiSelect({
-      selectedPanelFocusBoxShadow: '0 0 0 .1rem rgba(255, 213, 57, 0.25)',
-      selectedPanelFocusBorderColor: 'transparent'
-    });
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-36251023-1']);
-    _gaq.push(['_setDomainName', 'jqueryscript.net']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-
   }
 });
 
@@ -36,21 +21,6 @@ $.ajax({
   contentType: "application/json; charset=utf-8",
   success: function (jsondata) {
     tagsMeetup(jsondata.tags);
-    $('#tugsMeetup').bsMultiSelect({
-      selectedPanelFocusBoxShadow: '0 0 0 .1rem rgba(255, 213, 57, 0.25)',
-      selectedPanelFocusBorderColor: 'transparent'
-    });
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-36251023-1']);
-    _gaq.push(['_setDomainName', 'jqueryscript.net']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-
   }
 });
 
@@ -61,23 +31,6 @@ $.ajax({
   contentType: "application/json; charset=utf-8",
   success: function (jsondata) {
     industrySpeaker(jsondata);
-    $('#speakerIndustry').bsMultiSelect({
-      selectedPanelFocusBoxShadow: '0 0 0 .1rem rgba(255, 213, 57, 0.25)',
-      selectedPanelFocusBorderColor: 'transparent'
-    });
-
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-36251023-1']);
-    _gaq.push(['_setDomainName', 'jqueryscript.net']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-
-
   }
 });
 
@@ -88,23 +41,6 @@ $.ajax({
   contentType: "application/json; charset=utf-8",
   success: function (jsondata) {
     expertiseSpeaker(jsondata);
-    $('#speakerExpertise').bsMultiSelect({
-      selectedPanelFocusBoxShadow: '0 0 0 .1rem rgba(255, 213, 57, 0.25)',
-      selectedPanelFocusBorderColor: 'transparent'
-    });
-
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-36251023-1']);
-    _gaq.push(['_setDomainName', 'jqueryscript.net']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-
-
   }
 });
 
@@ -226,25 +162,6 @@ $.ajax({
   contentType: "application/json; charset=utf-8",
   success: function (jsondata) {
     allSpeakers(jsondata);
-    $('#speakerMeetup').bsMultiSelect({
-      selectedPanelFocusBoxShadow: '0 0 0 .1rem rgba(255, 213, 57, 0.25)',
-      selectedPanelFocusBorderColor: 'transparent',
-      selectedPanelClass: 'speaker-filtr-meetups form-control',
-      selectedItemId: 'qwer',
-    });
-
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-36251023-1']);
-    _gaq.push(['_setDomainName', 'jqueryscript.net']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-
-
   }
 });
 
