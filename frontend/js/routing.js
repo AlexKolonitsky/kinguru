b950832fdc55614bf759eea245ed7097d37f1cb2
OlegShulga <shulga.oleg@mifort.org>
2019-04-10 21:13:06 +0300

    change url

--- frontend/js/routing.js
+++ frontend/js/routing.js
@@ -1,1 +1,2 @@
-const urlBack = `http://ec2-34-251-80-45.eu-west-1.compute.amazonaws.com:3010`;
+// const urlBack = `http://ec2-34-251-80-45.eu-west-1.compute.amazonaws.com:3010`;
+const urlBack = 'https://kinguru.io';
