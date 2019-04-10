75981f62ce03a4215c9016f6cf688b32de034b35
iralitv <ira.litvinchuk@mifort.org>
2019-04-10 20:35:09 +0300

    adaptive layout for new_table 768px; 1024px

--- frontend/js/environmentProd.js
+++ frontend/js/environmentProd.js
@@ -1,2 +1,2 @@
-// const urlBack = `http://ec2-63-33-124-1.eu-west-1.compute.amazonaws.com:3010`;
+//const urlBack = `http://ec2-63-33-124-1.eu-west-1.compute.amazonaws.com:3010`;
 const urlBack = 'https://kinguru.io';
