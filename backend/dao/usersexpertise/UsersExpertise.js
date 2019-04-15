decb84b72e6cf4a726e23a71d7a6607aa8b964b3
OlegShulga <shulga.oleg@mifort.org>
2019-04-15 18:21:01 +0300

    add UserExpertise dao

---
+++ backend/dao/usersexpertise/UsersExpertise.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+
+  const UsersExpertise = sequelize.define('UsersKeywords', {
+
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    wordId: {
+      type: DataTypes.INTEGER,
+      references: 'Tags',
+      referencesKey: 'id',
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: 'Users',
+      referencesKey: 'id',
+    }
+  }, {
+
+    tableName: 'UsersExpertise',
+    timestamps: true,
+
+  });
+
+  return UsersExpertise;
+};
