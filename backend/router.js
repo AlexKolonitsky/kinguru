9beeca2934addb13244b7b95becd561e84cf78bb
OlegShulga <shulga.oleg@mifort.org>
2019-04-22 11:10:21 +0300

    change path for update images

--- backend/router.js
+++ backend/router.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer();
+const upload = multer('./images/');
 
 /**
  * @description Users endpoints
